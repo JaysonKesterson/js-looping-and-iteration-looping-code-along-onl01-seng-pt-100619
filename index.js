c644cab07199bc6660e0937794cfe6f183b23e99
Jayson Kesterson <jayson.kesterson24@gmail.com>
2020-04-22 21:38:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Code your solutions in this file
 
 function writeCards(array, name) {
+let newarray = [];
 for (let i = 0; i < array.length; i++) {
-    let newarray = [];
     newarray.push(`Thank you, ${array[i]}, for the wonderful ${name} gift!`)
   }
   return newarray;
