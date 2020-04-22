fe786f2db4c65412044b0ebc245d34436a8793d8
Jayson Kesterson <jayson.kesterson24@gmail.com>
2020-04-22 21:34:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solutions in this file
 
 function writeCards(array, name) {
-for (let i = 0; i < gifts.length; i++) {
+for (let i = 0; i < name.length; i++) {
     console.log(`Wrapped ${gifts[i]} and added a bow!`);
   }
 }
